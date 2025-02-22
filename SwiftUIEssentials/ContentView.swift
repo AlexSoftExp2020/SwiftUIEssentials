e8bb829862b6367befc2ee08185c95df2e87eb19
Alex <oleksii.pidhornyi@gmail.com>
2025-02-22 21:14:17 +0100

    Initial Commit

---
+++ SwiftUIEssentials/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 22.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIEssentials/SwiftUIEssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEssentialsApp.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 22.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
