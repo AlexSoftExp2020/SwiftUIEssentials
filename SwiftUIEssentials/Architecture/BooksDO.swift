945d3e7b52f1d617e22590ef1a18d5332afaa2d1
Alex <oleksii.pidhornyi@gmail.com>
2025-02-24 00:28:05 +0100

    add booksdo

---
+++ SwiftUIEssentials/Architecture/BooksDO.swift
@@ -0,0 +1,13 @@
+//
+//  BooksDO.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 24.02.2025.
+//
+
+import Foundation
+
+struct BooksDO: Identifiable {
+    let id = UUID()
+    let name: String
+}
