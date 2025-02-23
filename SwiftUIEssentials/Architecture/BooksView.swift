d61336dcbf4608311ea6bed674ad82dae7b1b77a
Alex <oleksii.pidhornyi@gmail.com>
2025-02-23 23:58:56 +0100

    add books view

---
+++ SwiftUIEssentials/Architecture/BooksView.swift
@@ -0,0 +1,19 @@
+//
+//  BooksView.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 23.02.2025.
+//
+
+import SwiftUI
+
+struct BooksView: View {
+    // @State private var oo = BooksOO()
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BooksView()
+}
