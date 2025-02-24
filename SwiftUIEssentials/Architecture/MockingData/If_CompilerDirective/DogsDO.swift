118e45ce3e747aa590477bb7f3bc4099b0fe9ba8
Alex <oleksii.pidhornyi@gmail.com>
2025-02-24 01:43:13 +0100

    add dogsdo

---
+++ SwiftUIEssentials/Architecture/MockingData/If_CompilerDirective/DogsDO.swift
@@ -0,0 +1,13 @@
+//
+//  DogsDO.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 24.02.2025.
+//
+
+import Foundation
+
+struct DogsDO: Identifiable {
+    let id = UUID()
+    var name: String
+}
