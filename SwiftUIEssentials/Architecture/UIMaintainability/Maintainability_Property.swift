12741f773d3dddfdd836b6610bebaeeecb9563fd
Alex <oleksii.pidhornyi@gmail.com>
2025-02-25 00:47:56 +0100

    extend maintability property

--- SwiftUIEssentials/Architecture/UIMaintainability/Maintainability_Property.swift
+++ SwiftUIEssentials/Architecture/UIMaintainability/Maintainability_Property.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct Maintainability_Property: View {
+    @State var oo = MockBookListOO()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                ForEach(oo.data) { datum in
+                    GroupBox {
+                        VStack {
+                            Image(systemName: "book.pages")
+                            Text(datum.name)
+                                .font(.title)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                    .listRowSeparator(.hidden)
+                }
+            } header: {
+                sectionHeaderView
+            }
+        }
+        .headerProminence(.increased)
+        .listStyle(.plain)
+        .onAppear {
+            oo.fetch()
+        }
+    }
+    
+    var sectionHeaderView: some View {
+        HStack {
+            Label("Books (\(oo.data.count))", systemImage: "books.vertical.fill")
+            Spacer()
+            Button("Add", systemImage: "plus") {
+                
+            }
+            .buttonStyle(.bordered)
+        }
     }
 }
 
