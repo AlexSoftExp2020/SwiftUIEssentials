a90f1ba7565a7f16405039d495d02a6c789b0f13
Alex <oleksii.pidhornyi@gmail.com>
2025-02-25 23:21:40 +0100

    extend maintainability view builder info

--- SwiftUIEssentials/Architecture/UIMaintainability/Maintainability_ViewBuilderIntro.swift
+++ SwiftUIEssentials/Architecture/UIMaintainability/Maintainability_ViewBuilderIntro.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct Maintainability_ViewBuilderIntro: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                GroupBox {
+                    Text("Employee Profile")
+                        .font(.title.weight(.bold).width(.compressed))
+                    Divider()
+                    HStack {
+                        Image(systemName: "person.circle")
+                            .font(.largeTitle)
+                        VStack(alignment: .leading) {
+                            Text("Susan McNall")
+                                .font(.title2.weight(.medium))
+                            Text("IT Department")
+                        }
+                        Spacer()
+                    }
+                }
+                .padding()
+                
+                GroupBox {
+                    Text("Expenses")
+                        .font(.title.weight(.bold).width(.compressed))
+                    Divider()
+                    LabeledContent("Flight", value: 580, format: .currency(code: "USD"))
+                    LabeledContent("Hotel", value: 1600, format: .currency(code: "USD"))
+                    LabeledContent("Meals", value: 418, format: .currency(code: "USD"))
+                }
+                .padding()
+            }
+        }
     }
 }
 
