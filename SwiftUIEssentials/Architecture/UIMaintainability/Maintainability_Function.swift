da6010f5850be79963c48041e44a51e78fec6925
Alex <oleksii.pidhornyi@gmail.com>
2025-02-25 22:24:44 +0100

    extends maintainability function

--- SwiftUIEssentials/Architecture/UIMaintainability/Maintainability_Function.swift
+++ SwiftUIEssentials/Architecture/UIMaintainability/Maintainability_Function.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct Maintainability_Function: View {
+    @State var oo = MockBookListOO()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                ForEach(oo.data) { datum in
+                    rowView(datum.name)
+                        .listRowSeparator(.hidden)
+                }
+            } header: {
+                sectionHeaderView
+            }
+        }
+        .headerProminence(.increased)
+        .listStyle(.plain)
+        .onAppear {
+            oo.fetch()
+        }
+    }
+    
+    var sectionHeaderView: some View {
+        HStack {
+            Label("Books (\(oo.data.count))", systemImage: "books.vertical.fill")
+            Spacer()
+            Button("Add", systemImage: "plus") {
+                
+            }
+            .buttonStyle(.bordered)
+        }
+    }
+    
+    func rowView(_ bookName: String) -> some View {
+        GroupBox {
+            VStack {
+                Image(systemName: "book.pages")
+                Text(bookName)
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
     }
 }
 
