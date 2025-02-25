8b89469ceac9f56b6b52f6689c41540f3c0a3c8a
Alex <oleksii.pidhornyi@gmail.com>
2025-02-25 22:19:23 +0100

    add maintainability function

---
+++ SwiftUIEssentials/Architecture/UIMaintainability/Maintainability_Function.swift
@@ -0,0 +1,18 @@
+//
+//  Maintainability_Function.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 25.02.2025.
+//
+
+import SwiftUI
+
+struct Maintainability_Function: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Maintainability_Function()
+}
