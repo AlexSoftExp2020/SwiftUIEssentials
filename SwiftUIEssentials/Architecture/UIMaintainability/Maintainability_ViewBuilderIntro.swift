d96a77212838218b168636ccf49742fc46549f34
Alex <oleksii.pidhornyi@gmail.com>
2025-02-25 23:08:18 +0100

    add maintainability view builder intro

---
+++ SwiftUIEssentials/Architecture/UIMaintainability/Maintainability_ViewBuilderIntro.swift
@@ -0,0 +1,18 @@
+//
+//  Maintainability_ViewBuilderIntro.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 25.02.2025.
+//
+
+import SwiftUI
+
+struct Maintainability_ViewBuilderIntro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Maintainability_ViewBuilderIntro()
+}
