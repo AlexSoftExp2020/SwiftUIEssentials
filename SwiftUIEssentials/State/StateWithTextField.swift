d2b7cc7100e300a05dfee37ac3b0688c2c40a0c5
Alex <oleksii.pidhornyi@gmail.com>
2025-02-28 00:48:46 +0100

    add state with text field

---
+++ SwiftUIEssentials/State/StateWithTextField.swift
@@ -0,0 +1,18 @@
+//
+//  StateWithTextField.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 28.02.2025.
+//
+
+import SwiftUI
+
+struct StateWithTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StateWithTextField()
+}
