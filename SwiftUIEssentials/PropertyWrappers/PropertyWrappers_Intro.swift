7d78f7ddf2c080a1c47eae71b06dbb763a62cc2f
Alex <oleksii.pidhornyi@gmail.com>
2025-02-27 22:41:39 +0100

    add property wrappers intro

---
+++ SwiftUIEssentials/PropertyWrappers/PropertyWrappers_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  PropertyWrappers_Intro.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 27.02.2025.
+//
+
+import SwiftUI
+
+struct PropertyWrappers_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PropertyWrappers_Intro()
+}
