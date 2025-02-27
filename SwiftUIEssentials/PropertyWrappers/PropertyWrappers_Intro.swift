3bf5d08cea566b1d642a477d531d4809187fc2bf
Alex <oleksii.pidhornyi@gmail.com>
2025-02-27 22:46:54 +0100

    add four characters property wrapper

--- SwiftUIEssentials/PropertyWrappers/PropertyWrappers_Intro.swift
+++ SwiftUIEssentials/PropertyWrappers/PropertyWrappers_Intro.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+@propertyWrapper
+struct FourCharacters {
+    private var value = ""
+    
+    var wrappedValue: String {
+        get { return value }
+        set { value = String(newValue.prefix(4)) }
+    }
+}
+
 struct PropertyWrappers_Intro: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
