e31fe62d22e249c99e1198b05596e1096b7cfd96
Alex <oleksii.pidhornyi@gmail.com>
2025-02-28 01:49:33 +0100

    extend header view

--- SwiftUIEssentials/StructProperties/CommonViews/HeaderView.swift
+++ SwiftUIEssentials/StructProperties/CommonViews/HeaderView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct HeaderView: View {
+    
+    var title: String
+    var subtitle: String
+    var desc: String
+    var back: Color
+    var textColor: Color
+    
+    init(_ title: String, subtitle: String, desc: String, back: Color, textColor: Color) {
+        self.title = title
+        self.subtitle = subtitle
+        self.desc = desc
+        self.back = back
+        self.textColor = textColor
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.light)
+                .foregroundStyle(.black)
+                .padding(.vertical)
+            
+            Text(subtitle)
+                .font(.title3)
+                .foregroundColor(.gray)
+            
+            Text(desc)
+                .font(.body)
+                .frame(maxWidth: .infinity)
+                .foregroundColor(textColor)
+                .padding()
+                .background(back)
+                
+        }
     }
 }
 
 #Preview {
-    HeaderView()
+    HeaderView("State",
+               subtitle: "Two-way binding",
+               desc: "add decription",
+               back: .blue,
+               textColor: .white)
 }
