dbd400a225b86b5d3ff8bb99615e571385cde72c
Alex <oleksii.pidhornyi@gmail.com>
2025-02-28 01:28:59 +0100

    add header view

---
+++ SwiftUIEssentials/StructProperties/CommonViews/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 28.02.2025.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
