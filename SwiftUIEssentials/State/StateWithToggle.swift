65ec90bf8fe9eb638e8381d409a215de98a918df
Alex <oleksii.pidhornyi@gmail.com>
2025-02-28 19:27:59 +0100

    add state with toggle

---
+++ SwiftUIEssentials/State/StateWithToggle.swift
@@ -0,0 +1,18 @@
+//
+//  StateWithToggle.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 28.02.2025.
+//
+
+import SwiftUI
+
+struct StateWithToggle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StateWithToggle()
+}
