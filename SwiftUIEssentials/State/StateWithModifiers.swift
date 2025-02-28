9450b89d8c891977f4faf5855d816d76963a7929
Alex <oleksii.pidhornyi@gmail.com>
2025-02-28 20:46:06 +0100

    add state with modifiers

---
+++ SwiftUIEssentials/State/StateWithModifiers.swift
@@ -0,0 +1,18 @@
+//
+//  StateWithModifiers.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 28.02.2025.
+//
+
+import SwiftUI
+
+struct StateWithModifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StateWithModifiers()
+}
