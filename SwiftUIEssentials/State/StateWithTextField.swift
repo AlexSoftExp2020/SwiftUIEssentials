41fea0d656085043b8d5499f8ebe6465e54a35c2
Alex <oleksii.pidhornyi@gmail.com>
2025-02-28 01:03:40 +0100

    extend state with text field

--- SwiftUIEssentials/State/StateWithTextField.swift
+++ SwiftUIEssentials/State/StateWithTextField.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct StateWithTextField: View {
+    @State var name = "Mariana"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            /*
+             HeaderView("State", subtitle: "Two-way binding", desc: "Add a dollar sign ($) before the variable name to create a two-way binding between a control and a state variable.", back: .blue, textColor: .white)
+             */
+            
+            Spacer()
+            TextField("Enter name", text: $name)
+                .textFieldStyle(.roundedBorder)
+                .padding(.horizontal)
+            
+            Text("Name:")
+            Text("\(name)")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
+            Spacer()
+            /*
+             DescView("A two-way binding means when the state variable is updated, the control gets updated. And when the control updates the value, the state variable gets updated.",
+             back: .blue,
+             textColor: .white)
+             */
+        }
+        .font(.title)
     }
 }
 
