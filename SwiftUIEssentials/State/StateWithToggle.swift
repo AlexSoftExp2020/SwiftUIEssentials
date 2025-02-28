f2a3fcd227fe1cc271b7f7b5ad6b8e8568050506
Alex <oleksii.pidhornyi@gmail.com>
2025-02-28 19:46:11 +0100

    extend state with toggle

--- SwiftUIEssentials/State/StateWithToggle.swift
+++ SwiftUIEssentials/State/StateWithToggle.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct StateWithToggle: View {
+    @State private var darkerBackground = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(darkerBackground ? .gray : .white)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                HeaderView("State",
+                           subtitle: "Two-way Binding",
+                           desc: "Many controls",
+                           back: .blue,
+                           textColor: .white)
+                
+                Spacer()
+                
+                Toggle(isOn: $darkerBackground) {
+                    Text("Darker Background: \(darkerBackground ? "ON" : "OFF")")
+                }
+                .padding()
+                
+                Spacer()
+                DescView("The ntire view is invalidated and redrawn when a state variable changes.",
+                         back: .blue,
+                         textColor: .white)
+            }
+            .font(.title)
+        }
     }
 }
 
