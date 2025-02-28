b4b86ee0567d1065f7da630b09a72d69116ef535
Alex <oleksii.pidhornyi@gmail.com>
2025-03-01 00:50:39 +0100

    add state with struct data

---
+++ SwiftUIEssentials/State/StateWithStructData.swift
@@ -0,0 +1,18 @@
+//
+//  StateWithStructData.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 28.02.2025.
+//
+
+import SwiftUI
+
+struct StateWithStructData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StateWithStructData()
+}
