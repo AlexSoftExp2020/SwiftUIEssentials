58ca7e334325d9e5a052acbfa197866def348851
Alex <oleksii.pidhornyi@gmail.com>
2025-03-02 00:37:12 +0100

    extend state with class data

--- SwiftUIEssentials/State/StateWithClassData.swift
+++ SwiftUIEssentials/State/StateWithClassData.swift
@@ -18,8 +18,34 @@ class NameClass {
 }
 
 struct StateWithClassData: View {
+    @State private var name = NameClass(first: "Fernanda", last: "Cruz")
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("State",
+                       subtitle: "Working With Classes",
+                       desc: "A class can be used for a one-way bind.",
+                       back: .blue,
+                       textColor: .white)
+            
+            Text("Full Name:")
+            Text("\(name.firstName) \(name.lastName)")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
+            VStack {
+                Text("Try updating the name:")
+                TextField("first name", text: $name.firstName)
+                TextField("last name", text: $name.lastName)
+            }
+            .textFieldStyle(.roundedBorder)
+            .padding()
+            Spacer()
+            DescView("But it won't work for two-way binds. Notice the screen isn't updating the name.",
+                     back: .blue,
+                     textColor: .white)
+        }
+        .font(.title)
     }
 }
 
