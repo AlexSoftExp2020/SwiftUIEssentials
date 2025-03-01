d5a70e3d1cc7e11c1c80fbdf9f2cf55398dd4bae
Alex <oleksii.pidhornyi@gmail.com>
2025-03-02 00:24:57 +0100

    add state with class data

--- SwiftUIEssentials/State/StateWithClassData.swift
+++ SwiftUIEssentials/State/StateWithClassData.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+class NameClass {
+    var firstName = ""
+    var lastName = ""
+    
+    init(first: String, last: String) {
+        self.firstName = first
+        self.lastName = last
+    }
+}
+
 struct StateWithClassData: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
