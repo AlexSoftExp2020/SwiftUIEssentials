371dda5967d2e6b3902bf25718b64ec1b453f873
Alex <oleksii.pidhornyi@gmail.com>
2025-03-01 01:06:54 +0100

    add name data and extend state with struct data

--- SwiftUIEssentials/State/StateWithStructData.swift
+++ SwiftUIEssentials/State/StateWithStructData.swift
@@ -7,9 +7,36 @@
 
 import SwiftUI
 
+struct NameData {
+    var firstName = ""
+    var lastName = ""
+}
+
 struct StateWithStructData: View {
+    @State private var nameData = NameData()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("State",
+                       subtitle: "Working With Structs",
+                       desc: "You can use a struct for your single source of truth.",
+                       back: .blue,
+                       textColor: .white)
+            Text("Full Name:")
+            Text("\(nameData.firstName) \(nameData.lastName)")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
+            Group {
+                Text("First and last names are both in the same struct:")
+                TextField("first name", text: $nameData.firstName)
+                TextField("last name", text: $nameData.lastName)
+            }
+            .textFieldStyle(.roundedBorder)
+            .padding(.horizontal)
+            Spacer()
+        }
+        .font(.title)
     }
 }
 
