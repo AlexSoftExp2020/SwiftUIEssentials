e71c9514d5e815f01b05b55c2cae3b1bc5fc01c5
Alex <oleksii.pidhornyi@gmail.com>
2025-03-01 23:53:36 +0100

    add state binding with other views

---
+++ SwiftUIEssentials/State/StateBindingWithOtherViews.swift
@@ -0,0 +1,18 @@
+//
+//  StateBindingWithOtherViews.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 01.03.2025.
+//
+
+import SwiftUI
+
+struct StateBindingWithOtherViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StateBindingWithOtherViews()
+}
