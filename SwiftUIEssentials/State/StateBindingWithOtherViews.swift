a35f18db7a4a117b1cac381720d5a3e70e416fca
Alex <oleksii.pidhornyi@gmail.com>
2025-03-02 00:01:03 +0100

    add binding to state examples

--- SwiftUIEssentials/State/StateBindingWithOtherViews.swift
+++ SwiftUIEssentials/State/StateBindingWithOtherViews.swift
@@ -7,6 +7,17 @@
 
 import SwiftUI
 
+struct BindingToStateExamples {
+    var color = Color.blue
+    var date = Date()
+    var slider = 0.75
+    var stepper = 25
+    var text = "This is a one-way bind"
+    var textField = "This is a two-way bind"
+    var textEditor = "TextEditor data"
+    var toggle = true
+}
+
 struct StateBindingWithOtherViews: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
