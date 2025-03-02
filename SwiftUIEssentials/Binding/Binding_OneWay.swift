e07884185f8de27f6e58f44f039c4d7249138b5c
Alex <oleksii.pidhornyi@gmail.com>
2025-03-02 01:31:38 +0100

    extend binding one way

---
+++ SwiftUIEssentials/Binding/Binding_OneWay.swift
@@ -0,0 +1,30 @@
+//
+//  Binding_OneWay.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 02.03.2025.
+//
+
+import SwiftUI
+
+struct Binding_OneWay: View {
+    @State private var name = "Sean Ching"
+    
+    var body: some View {
+        VStack(spacing: 20.0) {
+            Text("One-Way Binding")
+                .font(.largeTitle.bold())
+            
+            TextField("name", text: $name)
+                .textFieldStyle(.roundedBorder)
+            
+        // Namesubview
+        }
+        .font(.title)
+        .padding()
+    }
+}
+
+#Preview {
+    Binding_OneWay()
+}
