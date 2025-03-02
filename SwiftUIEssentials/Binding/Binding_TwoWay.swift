7f3dcb24ae6630a51c51015d8a657b87371f68ab
Alex <oleksii.pidhornyi@gmail.com>
2025-03-02 01:54:33 +0100

    add binding two way

---
+++ SwiftUIEssentials/Binding/Binding_TwoWay.swift
@@ -0,0 +1,18 @@
+//
+//  Binding_TwoWay.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 02.03.2025.
+//
+
+import SwiftUI
+
+struct Binding_TwoWay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Binding_TwoWay()
+}
