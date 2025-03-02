99c8912079b57c6653360ec04c4e32220f7dad42
Alex <oleksii.pidhornyi@gmail.com>
2025-03-03 00:25:12 +0100

    add binding list

---
+++ SwiftUIEssentials/Binding/Binding_List.swift
@@ -0,0 +1,18 @@
+//
+//  Binding_List.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 03.03.2025.
+//
+
+import SwiftUI
+
+struct Binding_List: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Binding_List()
+}
