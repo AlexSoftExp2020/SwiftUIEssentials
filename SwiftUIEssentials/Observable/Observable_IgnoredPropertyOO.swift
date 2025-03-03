70ee79375d1982cf97d2090951b41f6c039278c1
Alex <oleksii.pidhornyi@gmail.com>
2025-03-04 00:18:07 +0100

    add observable ignored propertyOO

---
+++ SwiftUIEssentials/Observable/Observable_IgnoredPropertyOO.swift
@@ -0,0 +1,18 @@
+//
+//  Observable_IgnoredProperty00.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 04.03.2025.
+//
+
+import SwiftUI
+
+struct Observable_IgnoredPropertyOO: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Observable_IgnoredPropertyOO()
+}
