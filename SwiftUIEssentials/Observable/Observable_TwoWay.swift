99aec75082fcd5b4f07177a21bf79e00089e4e4a
Alex <oleksii.pidhornyi@gmail.com>
2025-03-03 23:56:39 +0100

    add observable two way

---
+++ SwiftUIEssentials/Observable/Observable_TwoWay.swift
@@ -0,0 +1,18 @@
+//
+//  Observable_TwoWay.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 03.03.2025.
+//
+
+import SwiftUI
+
+struct Observable_TwoWay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Observable_TwoWay()
+}
