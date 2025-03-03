818ec25a49d5d453922c914c43130a6b573170a2
Alex <oleksii.pidhornyi@gmail.com>
2025-03-03 23:43:40 +0100

    add person class

--- SwiftUIEssentials/Observable/Observable_OneWay.swift
+++ SwiftUIEssentials/Observable/Observable_OneWay.swift
@@ -7,6 +7,10 @@
 
 import SwiftUI
 
+class PersonClass {
+     var name = ""
+}
+
 struct Observable_OneWay: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
