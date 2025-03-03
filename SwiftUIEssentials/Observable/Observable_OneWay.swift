c7a5d35070490de9edf389d70be457af72f25f98
Alex <oleksii.pidhornyi@gmail.com>
2025-03-03 23:42:46 +0100

    add observable one way

---
+++ SwiftUIEssentials/Observable/Observable_OneWay.swift
@@ -0,0 +1,18 @@
+//
+//  Observable_OneWay.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 03.03.2025.
+//
+
+import SwiftUI
+
+struct Observable_OneWay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Observable_OneWay()
+}
