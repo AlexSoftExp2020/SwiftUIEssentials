60d8b1f6daff54e3e1d92f955375d7400436cbff
Alex <oleksii.pidhornyi@gmail.com>
2025-03-03 23:57:35 +0100

    add person class two way

--- SwiftUIEssentials/Observable/Observable_TwoWay.swift
+++ SwiftUIEssentials/Observable/Observable_TwoWay.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+@Observable
+class PersonClass_TwoWay {
+    var name = " "
+}
+
 struct Observable_TwoWay: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
