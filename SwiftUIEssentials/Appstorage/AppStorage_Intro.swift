93fe1c84f5e0e51c6aa28d36e54d285e7a53b7fc
Alex <oleksii.pidhornyi@gmail.com>
2025-03-04 23:52:18 +0100

    add app storage intro

---
+++ SwiftUIEssentials/Appstorage/AppStorage_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  AppStorage_Intro.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 04.03.2025.
+//
+
+import SwiftUI
+
+struct AppStorage_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppStorage_Intro()
+}
