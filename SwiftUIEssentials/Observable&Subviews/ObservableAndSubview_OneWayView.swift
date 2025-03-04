df90377aa67344251b35290fb2af3b7b963fe6c9
Alex <oleksii.pidhornyi@gmail.com>
2025-03-04 14:21:49 +0100

    add observable and subview one way view

---
+++ SwiftUIEssentials/Observable&Subviews/ObservableAndSubview_OneWayView.swift
@@ -0,0 +1,18 @@
+//
+//  ObservableAndSubview_OneWayView.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 04.03.2025.
+//
+
+import SwiftUI
+
+struct ObservableAndSubview_OneWayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ObservableAndSubview_OneWayView()
+}
