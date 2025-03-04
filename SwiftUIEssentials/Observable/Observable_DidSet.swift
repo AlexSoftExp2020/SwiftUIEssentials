b543683ab1196dd128a2eee9f316d483f349c8c8
Alex <oleksii.pidhornyi@gmail.com>
2025-03-04 13:49:01 +0100

    add personoo

--- SwiftUIEssentials/Observable/Observable_DidSet.swift
+++ SwiftUIEssentials/Observable/Observable_DidSet.swift
@@ -7,6 +7,19 @@
 
 import SwiftUI
 
+class PersonOO {
+    var name = "" {
+        willSet {
+            // Happens BEFORE the property is changed to the new value.
+        }
+        didSet {
+            // Happens AFTER the property is set.
+            validationColor = name.isEmpty ? .red : .green
+        }
+    }
+    var validationColor = Color.red
+}
+
 struct Observable_DidSet: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
