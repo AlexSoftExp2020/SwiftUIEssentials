870eae43f4aca1bee1f635a85930257d5d36d081
Alex <oleksii.pidhornyi@gmail.com>
2025-03-04 13:09:13 +0100

    add observable did set

---
+++ SwiftUIEssentials/Observable/Observable_DidSet.swift
@@ -0,0 +1,18 @@
+//
+//  Observable_DidSet.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 04.03.2025.
+//
+
+import SwiftUI
+
+struct Observable_DidSet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Observable_DidSet()
+}
