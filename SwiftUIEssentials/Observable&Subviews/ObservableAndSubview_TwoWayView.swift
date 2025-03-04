4a8bf93998e19ebd7058879bfa4ac05317acd5e8
Alex <oleksii.pidhornyi@gmail.com>
2025-03-04 21:22:27 +0100

    add observable and subview two way view

---
+++ SwiftUIEssentials/Observable&Subviews/ObservableAndSubview_TwoWayView.swift
@@ -0,0 +1,18 @@
+//
+//  ObservableAndSubview_TwoWayView.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 04.03.2025.
+//
+
+import SwiftUI
+
+struct ObservableAndSubview_TwoWayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ObservableAndSubview_TwoWayView()
+}
