3d02045a8d8c3adbcf7322230e2ce439cd608695
Alex <oleksii.pidhornyi@gmail.com>
2025-03-06 00:21:28 +0100

    add app storage persistence

---
+++ SwiftUIEssentials/Appstorage/AppStorage_Persistence.swift
@@ -0,0 +1,18 @@
+//
+//  AppStorage_Persistence.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 06.03.2025.
+//
+
+import SwiftUI
+
+struct AppStorage_Persistence: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppStorage_Persistence()
+}
