db56e6f6bd7905d2223e9b22f589da071d9b341a
Alex <oleksii.pidhornyi@gmail.com>
2025-03-06 00:31:08 +0100

    extend app storage persistence

--- SwiftUIEssentials/Appstorage/AppStorage_Persistence.swift
+++ SwiftUIEssentials/Appstorage/AppStorage_Persistence.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct AppStorage_Persistence: View {
+    @AppStorage("darkBackground") var darkBakcground = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("AppStorage",
+                       subtitle: "Persistence",
+                       desc: "Data you store using @AppStorage will persist even after the app closes.",
+                       back: .orange,
+                       textColor: .black)
+            Toggle(isOn: $darkBakcground) {
+                Text("Use Dark Background?")
+            }
+            .padding()
+        }
+        .font(.title)
+        .preferredColorScheme(darkBakcground ? .dark : .light)
     }
 }
 
