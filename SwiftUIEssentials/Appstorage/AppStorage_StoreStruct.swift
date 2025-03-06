b82642dea82b037bd9b86b23ca5aa0505d7efe53
Alex <oleksii.pidhornyi@gmail.com>
2025-03-06 01:16:28 +0100

    add app storage store struct

---
+++ SwiftUIEssentials/Appstorage/AppStorage_StoreStruct.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 06.03.2025.
+//
+
+import SwiftUI
+
+struct AppStorage_StoreStruct: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppStorage_StoreStruct()
+}
