a6ddfed1453db797a379f5dbbf09eeef95f1f5e1
Alex <oleksii.pidhornyi@gmail.com>
2025-03-07 21:35:16 +0100

    extend saving data data entry

--- SwiftUIEssentials/SceneStorage/SavingData_DataEntry.swift
+++ SwiftUIEssentials/SceneStorage/SavingData_DataEntry.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct SavingData_DataEntry: View {
+    @SceneStorage("textInput") var textInput = ""
+    @State var stateText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            DescView("Enter data in here and then navigate away. It will be saved to the variable using @SceneStorage.",
+                     back: .pink,
+                     textColor: .black)
+            .layoutPriority(1)
+            TextEditor(text: $textInput)
+                .border(Color.pink)
+                .padding()
+            
+            DescView("This data will not be saved because it's bound to a @State variable.",
+                     back: .pink,
+                     textColor: .black)
+            TextEditor(text: $stateText)
+                .border(Color.pink)
+                .padding()
+        }
+        .font(.title)
     }
 }
 
