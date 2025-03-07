5d56e528d6aab0d999708d01abc7277ccfa68256
Alex <oleksii.pidhornyi@gmail.com>
2025-03-07 23:55:35 +0100

    add developerOO

--- SwiftUIEssentials/Environment/Environment_Intro.swift
+++ SwiftUIEssentials/Environment/Environment_Intro.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+@Observable
+class DeveloperOO {
+    var name: String = "Awesome developer"
+}
+
 struct Environment_Intro: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
