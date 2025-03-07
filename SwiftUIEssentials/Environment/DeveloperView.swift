8cbbe45a188ec039a5c87c839167582e96370173
Alex <oleksii.pidhornyi@gmail.com>
2025-03-08 00:18:20 +0100

    add developer view

---
+++ SwiftUIEssentials/Environment/DeveloperView.swift
@@ -0,0 +1,21 @@
+//
+//  DeveloperView.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 08.03.2025.
+//
+
+import SwiftUI
+
+struct DeveloperView: View {
+    @Environment(DeveloperOO.self) private var dev
+    
+    var body: some View {
+        Text("Hello, \(dev.name)")
+            .navigationTitle("Developer View")
+    }
+}
+
+#Preview {
+    DeveloperView()
+}
