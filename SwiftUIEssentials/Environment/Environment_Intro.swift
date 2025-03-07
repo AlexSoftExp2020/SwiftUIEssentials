28092b3f417723254e51508d857b7a911c48345b
Alex <oleksii.pidhornyi@gmail.com>
2025-03-07 23:53:12 +0100

    add environment intro

---
+++ SwiftUIEssentials/Environment/Environment_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  Environment_Intro.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 07.03.2025.
+//
+
+import SwiftUI
+
+struct Environment_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Environment_Intro()
+}
