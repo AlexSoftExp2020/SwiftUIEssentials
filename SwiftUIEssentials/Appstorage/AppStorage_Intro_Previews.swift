6330791cb0c89db59c50b1596574ec39392718b2
Alex <oleksii.pidhornyi@gmail.com>
2025-03-07 13:40:47 +0100

    add app storage intro previews

---
+++ SwiftUIEssentials/Appstorage/AppStorage_Intro_Previews.swift
@@ -0,0 +1,20 @@
+//
+//  AppStorage_Intro_Previews.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 07.03.2025.
+//
+
+import SwiftUI
+
+struct AppStorage_Intro_Previews: PreviewProvider {
+    @AppStorage("username") static var username: String!
+    
+    static var previews: some View {
+        username = "really_really_really_really_long_username"
+        return Group {
+            AppStorage_Intro()
+            AppStorage_Edit()
+        }
+    }
+}
