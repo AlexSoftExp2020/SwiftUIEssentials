b0243ce92b2712cbb5b169012a6ffe6b34d4ec41
Alex <oleksii.pidhornyi@gmail.com>
2025-03-07 13:51:44 +0100

    add comment to app storage

---
+++ SwiftUIEssentials/SceneStorage/SavingData_SceneStorage.swift
@@ -0,0 +1,18 @@
+//
+//  SavingData_SceneStorage.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 07.03.2025.
+//
+
+import SwiftUI
+
+struct SavingData_SceneStorage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavingData_SceneStorage()
+}
