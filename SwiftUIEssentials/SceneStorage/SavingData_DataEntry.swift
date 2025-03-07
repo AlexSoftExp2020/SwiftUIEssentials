79fdd2dc74ac41c9dfc0a352044d3d3177fd7752
Alex <oleksii.pidhornyi@gmail.com>
2025-03-07 21:25:01 +0100

    add saving data data entry

---
+++ SwiftUIEssentials/SceneStorage/SavingData_DataEntry.swift
@@ -0,0 +1,18 @@
+//
+//  SavingData_DataEntry.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 07.03.2025.
+//
+
+import SwiftUI
+
+struct SavingData_DataEntry: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavingData_DataEntry()
+}
