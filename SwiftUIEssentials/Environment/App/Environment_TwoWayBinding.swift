dde247f877c477431fe580be56f52a92cd64445b
Alex <oleksii.pidhornyi@gmail.com>
2025-03-09 00:29:34 +0100

    extend environment two way binding

--- SwiftUIEssentials/Environment/App/Environment_TwoWayBinding.swift
+++ SwiftUIEssentials/Environment/App/Environment_TwoWayBinding.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct Environment_TwoWayBinding: View {
+    @Environment(AddressOO.self) private var addressOO
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            @Bindable var addressBindable = addressOO
+            Section("One-Way Binding") {
+                Text("State: \(addressOO.state)")
+                    .bold()
+            }
+            
+            Section("Two-Way Binding:") {
+                TextField("Enter State", text: $addressBindable.state)
+            }
+        }
+        .headerProminence(.increased)
     }
 }
 
 #Preview {
     Environment_TwoWayBinding()
+        .environment(AddressOO())
 }
