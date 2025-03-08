e0f657be4def2708df77900b5d98bd256d833480
Alex <oleksii.pidhornyi@gmail.com>
2025-03-08 12:02:25 +0100

    add environment two views deep

---
+++ SwiftUIEssentials/Environment/Environment_TwoViewsDeep.swift
@@ -0,0 +1,18 @@
+//
+//  Environment_TwoViewsDeep.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 08.03.2025.
+//
+
+import SwiftUI
+
+struct Environment_TwoViewsDeep: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Environment_TwoViewsDeep()
+}
