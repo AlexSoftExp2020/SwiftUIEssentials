b25febd0c6f347ff1dc96e90c5ac5ae70dff33c8
Alex <oleksii.pidhornyi@gmail.com>
2025-03-08 18:08:02 +0100

    extend developer intro view

--- SwiftUIEssentials/Environment/DeveloperIntroView.swift
+++ SwiftUIEssentials/Environment/DeveloperIntroView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct DeveloperIntroView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink("Go to View 3") {
+            DeveloperView()
+        }
+        .navigationTitle("View 2")
     }
 }
 
