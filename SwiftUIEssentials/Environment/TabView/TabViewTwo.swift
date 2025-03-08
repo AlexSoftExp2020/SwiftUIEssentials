245f4eb08511ef5aa05444233df46d60223ac270
Alex <oleksii.pidhornyi@gmail.com>
2025-03-08 19:09:07 +0100

    add tab view two

---
+++ SwiftUIEssentials/Environment/TabView/TabViewTwo.swift
@@ -0,0 +1,18 @@
+//
+//  TabViewTwo.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 08.03.2025.
+//
+
+import SwiftUI
+
+struct TabViewTwo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabViewTwo()
+}
