114a8e8709050962fb8f4b7b3dbf7ead31e115dd
Alex <oleksii.pidhornyi@gmail.com>
2025-03-08 18:19:55 +0100

    extend environment two views deep

--- SwiftUIEssentials/Environment/Environment_TwoViewsDeep.swift
+++ SwiftUIEssentials/Environment/Environment_TwoViewsDeep.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct Environment_TwoViewsDeep: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            NavigationLink("Go to View 2") {
+                DeveloperIntroView()
+            }
+            .navigationTitle("Developer")
+        }
+        .environment(DeveloperOO())
+        .font(.title)
     }
 }
 
