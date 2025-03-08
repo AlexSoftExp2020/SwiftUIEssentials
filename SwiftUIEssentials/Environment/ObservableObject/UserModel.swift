a75775c7334c2e7c9b2128175cd42f7996787834
Alex <oleksii.pidhornyi@gmail.com>
2025-03-08 19:35:59 +0100

    add user model

---
+++ SwiftUIEssentials/Environment/ObservableObject/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 08.03.2025.
+//
+
+import Foundation
+
+@Observable
+class UserModel {
+    var username = "Ellen"
+    var showOnLine = true
+}
