2f3e474029fb6cc2dfda9888c163122e9a73f598
Alex <oleksii.pidhornyi@gmail.com>
2025-03-08 18:50:40 +0100

    add tab view one

---
+++ SwiftUIEssentials/Environment/TabView/TabViewOne.swift
@@ -0,0 +1,18 @@
+//
+//  TabViewOne.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 08.03.2025.
+//
+
+import SwiftUI
+
+struct TabViewOne: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabViewOne()
+}
