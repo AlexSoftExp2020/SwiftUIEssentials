926accf8cf3bcd314999978d183cc629deb17c20
Alex <oleksii.pidhornyi@gmail.com>
2025-03-08 18:44:05 +0100

    refactor environment tab view

--- SwiftUIEssentials/Environment/TabView/Environment_TabView.swift
+++ SwiftUIEssentials/Environment/TabView/Environment_TabView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct Environment_TabView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            //MARK: TODO
+        }
     }
 }
 
