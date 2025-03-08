b7bf5fc4a0f562827903f4e6aaf3d89dd61881ac
Alex <oleksii.pidhornyi@gmail.com>
2025-03-08 23:53:44 +0100

    extend environment no available in init

--- SwiftUIEssentials/Environment/App/Environment_NotAvailableInInit.swift
+++ SwiftUIEssentials/Environment/App/Environment_NotAvailableInInit.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct Environment_NotAvailableInInit: View {
+    @Environment(UserOO.self) var user
+    @State private var highlight = false
+//    init() {
+//        // Environment objects are not accessible in the init.
+//        if user.userName == "scott.smith" {
+//            highlight = true
+//        }
+//    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(user.userName)
+            .padding()
+            .background(highlight ? Color.yellow : Color.clear)
+            .font(.title)
     }
 }
 
 #Preview {
     Environment_NotAvailableInInit()
+        .environment(UserOO())
 }
