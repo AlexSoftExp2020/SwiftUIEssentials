0ba53c07e6e81400d53e04b354cfa813e36da87e
Alex <oleksii.pidhornyi@gmail.com>
2025-03-08 19:15:43 +0100

    extend tab view two

--- SwiftUIEssentials/Environment/TabView/TabViewTwo.swift
+++ SwiftUIEssentials/Environment/TabView/TabViewTwo.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct TabViewTwo: View {
+    @Environment(DeveloperOO.self) var developerInfo
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Tab 2")
+                .font(.largeTitle)
+            Spacer()
+            Text("The name on Tab 1 is:")
+                .padding()
+            Text("\(developerInfo.name)")
+                .bold()
+            Spacer()
+        }
     }
 }
 
 #Preview {
     TabViewTwo()
+        .environment(DeveloperOO())
 }
