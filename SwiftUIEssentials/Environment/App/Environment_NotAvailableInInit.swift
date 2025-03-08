ef2e5758a4b7648d7effd2968883472c79631e0f
Alex <oleksii.pidhornyi@gmail.com>
2025-03-08 23:32:58 +0100

    add environment not available in init

---
+++ SwiftUIEssentials/Environment/App/Environment_NotAvailableInInit.swift
@@ -0,0 +1,18 @@
+//
+//  Environment_NotAvailableInInit.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 08.03.2025.
+//
+
+import SwiftUI
+
+struct Environment_NotAvailableInInit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Environment_NotAvailableInInit()
+}
