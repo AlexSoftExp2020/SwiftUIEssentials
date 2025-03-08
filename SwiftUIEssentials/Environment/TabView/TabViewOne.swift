1c1c45c08b0a9867765c484590803e290302c0a0
Alex <oleksii.pidhornyi@gmail.com>
2025-03-08 19:00:33 +0100

    extend tab view one

--- SwiftUIEssentials/Environment/TabView/TabViewOne.swift
+++ SwiftUIEssentials/Environment/TabView/TabViewOne.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct TabViewOne: View {
+    @Environment(DeveloperOO.self) var devInfo
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Tab 1")
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            Text("\(devInfo.name)")
+            
+            Button("Update") {
+                devInfo.name = "New Awesome Dev"
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Spacer()
+        }
     }
 }
 
 #Preview {
     TabViewOne()
+        .environment(DeveloperOO())
 }
