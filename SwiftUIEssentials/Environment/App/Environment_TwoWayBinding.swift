e0c22cf395e465c7f022202911e28b0c039297ec
Alex <oleksii.pidhornyi@gmail.com>
2025-03-09 00:27:05 +0100

    add environment two way binding

---
+++ SwiftUIEssentials/Environment/App/Environment_TwoWayBinding.swift
@@ -0,0 +1,18 @@
+//
+//  Environment_TwoWayBinding.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 09.03.2025.
+//
+
+import SwiftUI
+
+struct Environment_TwoWayBinding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Environment_TwoWayBinding()
+}
