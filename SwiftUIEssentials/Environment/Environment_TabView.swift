563f1ad7ecae9fb0b57ce8de2de0d893b3c92a22
Alex <oleksii.pidhornyi@gmail.com>
2025-03-08 18:41:08 +0100

    add environment tab view

---
+++ SwiftUIEssentials/Environment/Environment_TabView.swift
@@ -0,0 +1,18 @@
+//
+//  Environment_TabView.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 08.03.2025.
+//
+
+import SwiftUI
+
+struct Environment_TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Environment_TabView()
+}
