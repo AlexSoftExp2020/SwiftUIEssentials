a98e994455e1ddac4293b6486a3ffaed0b0a0361
Alex <oleksii.pidhornyi@gmail.com>
2025-03-08 17:55:50 +0100

    add developer intro view

---
+++ SwiftUIEssentials/Environment/DeveloperIntroView.swift
@@ -0,0 +1,18 @@
+//
+//  DeveloperIntroView.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 08.03.2025.
+//
+
+import SwiftUI
+
+struct DeveloperIntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeveloperIntroView()
+}
