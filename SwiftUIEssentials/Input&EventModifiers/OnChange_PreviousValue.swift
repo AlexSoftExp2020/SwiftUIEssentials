b09ba619a519b5a15ae470171f1ff852142cd08a
Alex <oleksii.pidhornyi@gmail.com>
2025-03-09 22:47:05 +0100

    extend on change previous value

--- SwiftUIEssentials/Input&EventModifiers/OnChange_PreviousValue.swift
+++ SwiftUIEssentials/Input&EventModifiers/OnChange_PreviousValue.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct OnChange_PreviousValue: View {
+    @State private var rotation = Angle.degrees(0)
+    @State private var size: CGFloat = 50
+    @State private var color = Color.green
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("onChange",
+                       subtitle: "Previous Value",
+                       desc: "The onChange modifier can also view the previous value of the property that is being changed.",
+                       back: .blue,
+                       textColor: .white)
+            HStack(spacing: 75) {
+                Button("Reset") { rotation = Angle.degrees(0) }
+                Button("Rotate") { rotation = Angle.degrees(90) }
+            }
+            Spacer()
+            Image(systemName: "l.joystick.tilt.up.fill")
+                .rotationEffect(rotation)
+                .font(.system(size: size))
+                .foregroundStyle(color)
+                .animation(.default, value: rotation)
+            Spacer()
+        }
+        .font(.title)
+        .onChange(of: rotation) { oldValue, newValue in
+            print("Previous Value: \(oldValue)")
+            if newValue == Angle.degrees(0) {
+                size = 50
+                color = Color.green
+            } else {
+                size = 100
+                color = Color.red
+            }
+        }
     }
 }
 
