55bb147b9901e533eacac4837ccbb0e5ed9e38dd
Alex <oleksii.pidhornyi@gmail.com>
2025-03-09 13:50:55 +0100

    add on change intro

---
+++ SwiftUIEssentials/Input&EventModifiers/OnChange_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  OnChange_Intro.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 09.03.2025.
+//
+
+import SwiftUI
+
+struct OnChange_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnChange_Intro()
+}
