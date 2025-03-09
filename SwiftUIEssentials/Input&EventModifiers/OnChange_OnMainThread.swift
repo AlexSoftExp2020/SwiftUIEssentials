f2e11b421cdce1c22447b358941b42e8451f5a80
Alex <oleksii.pidhornyi@gmail.com>
2025-03-09 14:12:49 +0100

    add on change on main thread

---
+++ SwiftUIEssentials/Input&EventModifiers/OnChange_OnMainThread.swift
@@ -0,0 +1,18 @@
+//
+//  OnChange_OnMainThread.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 09.03.2025.
+//
+
+import SwiftUI
+
+struct OnChange_OnMainThread: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnChange_OnMainThread()
+}
