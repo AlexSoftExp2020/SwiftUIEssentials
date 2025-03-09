f94e71a3d9bafb24533447315e99a41f975d6348
Alex <oleksii.pidhornyi@gmail.com>
2025-03-09 22:34:44 +0100

    add on change previous value

---
+++ SwiftUIEssentials/Input&EventModifiers/OnChange_PreviousValue.swift
@@ -0,0 +1,18 @@
+//
+//  OnChange_PreviousValue.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 09.03.2025.
+//
+
+import SwiftUI
+
+struct OnChange_PreviousValue: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnChange_PreviousValue()
+}
