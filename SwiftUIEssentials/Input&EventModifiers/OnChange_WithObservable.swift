a3580940c3fb962ec8d11ca54ad6c6955ed00639
Alex <oleksii.pidhornyi@gmail.com>
2025-03-10 23:45:29 +0100

    add on change with observable

---
+++ SwiftUIEssentials/Input&EventModifiers/OnChange_WithObservable.swift
@@ -0,0 +1,18 @@
+//
+//  OnChange_WithObservable.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 10.03.2025.
+//
+
+import SwiftUI
+
+struct OnChange_WithObservable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnChange_WithObservable()
+}
