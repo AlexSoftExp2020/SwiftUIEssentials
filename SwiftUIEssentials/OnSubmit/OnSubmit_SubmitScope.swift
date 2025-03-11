50c945db2efae3e60683d8bd132b5b15b137017a
Alex <oleksii.pidhornyi@gmail.com>
2025-03-11 19:02:46 +0100

    add on submit submit scope

---
+++ SwiftUIEssentials/OnSubmit/OnSubmit_SubmitScope.swift
@@ -0,0 +1,18 @@
+//
+//  OnSubmit_SubmitScope.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 11.03.2025.
+//
+
+import SwiftUI
+
+struct OnSubmit_SubmitScope: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnSubmit_SubmitScope()
+}
