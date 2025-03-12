51b2d57be1037e664af7c39a0ab3404881b6f350
Alex <oleksii.pidhornyi@gmail.com>
2025-03-12 23:37:11 +0100

    add json user data and extend json objects oo

--- SwiftUIEssentials/WorkingWithJSON/ObservableObject/Json_ObjectsOO.swift
+++ SwiftUIEssentials/WorkingWithJSON/ObservableObject/Json_ObjectsOO.swift
@@ -9,5 +9,28 @@ import Foundation
 
 @Observable
 class Json_ObjectsOO {
-    //MARK: TODO
+    var user = Json_UserWithAddress()
+    var jsonError: Error?
+    
+    func fetch() {
+        let jsonData = jsonUserData.data(using: .utf8)!
+        
+        do {
+            user = try JSONDecoder().decode(Json_UserWithAddress.self, from: jsonData)
+        } catch {
+            jsonError = error
+        }
+    }
 }
+
+var jsonUserData = """
+{
+"name": "Max Acavard",
+"address": {
+"street": "73 School St",
+"city": "Franconia",
+"state": "NH",
+"zipcode": "03580"
+}
+}
+"""
