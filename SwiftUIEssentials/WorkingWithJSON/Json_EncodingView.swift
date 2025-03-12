80aa36c16e19059c15f32f19c8218e0bbd79303e
Alex <oleksii.pidhornyi@gmail.com>
2025-03-12 22:58:26 +0100

    extend json encoding view

--- SwiftUIEssentials/WorkingWithJSON/Json_EncodingView.swift
+++ SwiftUIEssentials/WorkingWithJSON/Json_EncodingView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct Json_EncodingView: View {
+    @State private var oo = Json_Encoding()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("JSON",
+                       subtitle: "Encoding",
+                       desc: "JSON encoding is the process of taking a struct or class and converting it to JSON data.",
+                       back: .blue,
+                       textColor: .white)
+            
+            Button("Save to JSON") {
+                oo.save()
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Text(oo.formattedJson)
+                .font(.system(.title, design: .monospaced))
+            
+            Spacer()
+            
+            Text(oo.jsonError?.localizedDescription ?? "")
+        }
+        .onAppear {
+            oo.fetch()
+        }
+        .font(.title)
     }
 }
 
