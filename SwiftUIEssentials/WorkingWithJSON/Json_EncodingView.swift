ba59ca588d49cedc7dbee7acce719ce58273d4f7
Alex <oleksii.pidhornyi@gmail.com>
2025-03-12 22:48:03 +0100

    add json encoding view

---
+++ SwiftUIEssentials/WorkingWithJSON/Json_EncodingView.swift
@@ -0,0 +1,18 @@
+//
+//  Json_EncodingView.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 12.03.2025.
+//
+
+import SwiftUI
+
+struct Json_EncodingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Json_EncodingView()
+}
