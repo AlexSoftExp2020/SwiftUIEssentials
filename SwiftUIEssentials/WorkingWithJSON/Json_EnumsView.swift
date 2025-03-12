5f4bd317413da791380d896d493479d179465921
Alex <oleksii.pidhornyi@gmail.com>
2025-03-13 00:43:22 +0100

    extend json enums view

--- SwiftUIEssentials/WorkingWithJSON/Json_EnumsView.swift
+++ SwiftUIEssentials/WorkingWithJSON/Json_EnumsView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct Json_EnumsView: View {
+    @State private var oo = Json_EnumsOO()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("JSON",
+                       subtitle: "Enums",
+                       desc: "Sometimes a field in JSON can only be a set number of values. In this case, you might want to decode that value into an enum.",
+                       back: .blue,
+                       textColor: .white)
+            Text(oo.carData.name)
+            Text(oo.carData.type.rawValue.capitalized)
+            Text(oo.jsonError?.localizedDescription ?? "")
+        }
+        .onAppear {
+            oo.fetch()
+        }
+        .font(.title)
     }
 }
 
