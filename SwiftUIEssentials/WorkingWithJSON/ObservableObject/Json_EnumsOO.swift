93913c1016996621b216550528148147fbd4b056
Alex <oleksii.pidhornyi@gmail.com>
2025-03-13 00:10:57 +0100

    add json enums oo

---
+++ SwiftUIEssentials/WorkingWithJSON/ObservableObject/Json_EnumsOO.swift
@@ -0,0 +1,18 @@
+//
+//  Json_EnumsOO.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 13.03.2025.
+//
+
+import SwiftUI
+
+@Observable
+class Json_EnumsOO {
+    var carData = Json_Car()
+    var jsonError: Error?
+    
+    func fetch() {
+        //MARK: TODO
+    }
+}
