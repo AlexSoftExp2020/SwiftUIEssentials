c3dfbaf09f340f53e77feeb969e8e06fdf85c221
Alex <oleksii.pidhornyi@gmail.com>
2025-03-12 23:39:04 +0100

    add json objects view

---
+++ SwiftUIEssentials/WorkingWithJSON/Json_ObjectsView.swift
@@ -0,0 +1,18 @@
+//
+//  Json_ObjectsView.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 12.03.2025.
+//
+
+import SwiftUI
+
+struct Json_ObjectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Json_ObjectsView()
+}
