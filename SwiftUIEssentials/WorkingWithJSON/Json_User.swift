72eb7b31b932060b478fa0b17f1f5793ef65d0e3
Alex <oleksii.pidhornyi@gmail.com>
2025-03-12 19:57:49 +0100

    add json user

---
+++ SwiftUIEssentials/WorkingWithJSON/Json_User.swift
@@ -0,0 +1,14 @@
+//
+//  Json_User.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 12.03.2025.
+//
+
+struct Json_User: Decodable {
+    var name = ""
+    var age = 0
+    var married = false
+    var pets: [String] = []
+    var email: String?
+}
