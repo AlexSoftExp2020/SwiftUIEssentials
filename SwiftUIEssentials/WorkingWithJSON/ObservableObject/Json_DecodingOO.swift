2d13d67fd26a41503aaa8b2f7dcf31a989a06803
Alex <oleksii.pidhornyi@gmail.com>
2025-03-12 20:21:05 +0100

    add json decoding oo

---
+++ SwiftUIEssentials/WorkingWithJSON/ObservableObject/Json_DecodingOO.swift
@@ -0,0 +1,18 @@
+//
+//  Json_DecodingOO.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 12.03.2025.
+//
+
+import Foundation
+
+@Observable
+class Json_DecodingOO {
+    var user = Json_User()
+    var jsonError: Error?
+    
+    func fetch() {
+        
+    }
+}
