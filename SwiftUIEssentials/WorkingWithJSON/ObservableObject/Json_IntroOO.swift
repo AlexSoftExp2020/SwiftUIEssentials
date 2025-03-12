5a576511396893f36fd5418760b279b57589ee82
Alex <oleksii.pidhornyi@gmail.com>
2025-03-12 19:35:01 +0100

    add json introoo observable object

---
+++ SwiftUIEssentials/WorkingWithJSON/ObservableObject/Json_IntroOO.swift
@@ -0,0 +1,17 @@
+//
+//  Json_IntroOO.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 12.03.2025.
+//
+
+import SwiftUI
+
+@Observable
+class Json_IntroOO {
+    var data = ""
+    
+    func fetch() {
+        data = userJsonData
+    }
+}
