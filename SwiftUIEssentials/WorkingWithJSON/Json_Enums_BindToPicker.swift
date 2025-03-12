d69750517c26a8890025a30094bb956c1ce97116
Alex <oleksii.pidhornyi@gmail.com>
2025-03-13 00:47:07 +0100

    add json enums bind to picker

---
+++ SwiftUIEssentials/WorkingWithJSON/Json_Enums_BindToPicker.swift
@@ -0,0 +1,18 @@
+//
+//  Json_Enums_BindToPicker.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 13.03.2025.
+//
+
+import SwiftUI
+
+struct Json_Enums_BindToPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Json_Enums_BindToPicker()
+}
