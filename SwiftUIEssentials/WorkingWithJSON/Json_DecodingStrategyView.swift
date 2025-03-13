ac3f3c91ff855dc6dbc17bbb71bc8c174a0c1348
Alex <oleksii.pidhornyi@gmail.com>
2025-03-13 18:34:03 +0100

    add json decoding strategy view

---
+++ SwiftUIEssentials/WorkingWithJSON/Json_DecodingStrategyView.swift
@@ -0,0 +1,18 @@
+//
+//  Json_DecodingStrategyView.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 13.03.2025.
+//
+
+import SwiftUI
+
+struct Json_DecodingStrategyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Json_DecodingStrategyView()
+}
