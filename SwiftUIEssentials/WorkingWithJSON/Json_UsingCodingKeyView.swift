bab8bfd372c6e286c47923ef5f13dabd89a8b1e6
Alex <oleksii.pidhornyi@gmail.com>
2025-03-13 19:09:13 +0100

    add json using coding key view

---
+++ SwiftUIEssentials/WorkingWithJSON/Json_UsingCodingKeyView.swift
@@ -0,0 +1,18 @@
+//
+//  Json_UsingCodingKeyView.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 13.03.2025.
+//
+
+import SwiftUI
+
+struct Json_UsingCodingKeyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Json_UsingCodingKeyView()
+}
