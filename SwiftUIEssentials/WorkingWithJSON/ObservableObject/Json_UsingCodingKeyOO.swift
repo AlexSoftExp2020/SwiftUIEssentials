79a4f3905dfd5742559d912fe7233ee130aa7d2b
Alex <oleksii.pidhornyi@gmail.com>
2025-03-13 19:00:29 +0100

    add json vegetable data

---
+++ SwiftUIEssentials/WorkingWithJSON/ObservableObject/Json_UsingCodingKeyOO.swift
@@ -0,0 +1,16 @@
+//
+//  Json_UsingCodingKeyOO.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 13.03.2025.
+//
+
+import Foundation
+
+var jsonVegetableData = """
+{
+"01_vegetable_name": "Beans",
+"02_vegetable_color": "Yellow",
+"quantity": 10
+}
+"""
