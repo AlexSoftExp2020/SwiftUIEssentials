3868a5b74e33f44119758bd3110cf128ffb596af
Alex <oleksii.pidhornyi@gmail.com>
2025-03-13 19:16:21 +0100

    extend json using coding key view

--- SwiftUIEssentials/WorkingWithJSON/Json_UsingCodingKeyView.swift
+++ SwiftUIEssentials/WorkingWithJSON/Json_UsingCodingKeyView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct Json_UsingCodingKeyView: View {
+    @State private var oo = Json_UsingCodingKeyOO()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HeaderView("JSON",
+                       subtitle: "Using CodingKey",
+                       desc: "In some cases you get names in JSON that do not match up to your destination object at all. Use a CodingKeys enum to map them.",
+                       back: .blue,
+                       textColor: .white)
+            
+            Text("Name: \(oo.vegetable.name)")
+            Text("Color: \(oo.vegetable.color)")
+            Text("Quantity: \(oo.vegetable.quantity)")
+            Text(oo.jsonError?.localizedDescription ?? "")
+        }
+        .onAppear {
+            oo.fetch()
+        }
+        .font(.title)
     }
 }
 
