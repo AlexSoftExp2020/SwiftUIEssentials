808676d00d3cfd573760a5d4785f10fd822274d1
Alex <oleksii.pidhornyi@gmail.com>
2025-03-13 19:36:44 +0100

    add json todo data

---
+++ SwiftUIEssentials/WorkingWithJSON/ObservableObjects/Json_DatesOO.swift
@@ -0,0 +1,20 @@
+//
+//  Json_DatesOO.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 13.03.2025.
+//
+
+import Foundation
+
+var jsonTodoData = """
+{
+    "tasks": [
+        {
+            "id": 1,
+            "description": "Buy birthday present",
+            "dueDate": "2024-06-24T19:00:57Z"
+        }
+    ]
+}
+"""
