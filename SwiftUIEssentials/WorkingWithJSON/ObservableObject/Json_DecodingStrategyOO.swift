2aead10bbe85b73516c57b91a2231b14d2c86054
Alex <oleksii.pidhornyi@gmail.com>
2025-03-13 18:15:19 +0100

    add json fruit data

---
+++ SwiftUIEssentials/WorkingWithJSON/ObservableObject/Json_DecodingStrategyOO.swift
@@ -0,0 +1,15 @@
+//
+//  Json_DecodingStrategyOO.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 13.03.2025.
+//
+
+import Foundation
+
+var jsonFruitData = """
+{
+"fruit_name": "Apple",
+"fruit_color": "Green"
+}
+"""
