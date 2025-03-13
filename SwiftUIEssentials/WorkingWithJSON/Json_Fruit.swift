f134c1e46ff28b5a035bf58f3af9e40004b58e7e
Alex <oleksii.pidhornyi@gmail.com>
2025-03-13 18:23:22 +0100

    add json fruit

---
+++ SwiftUIEssentials/WorkingWithJSON/Json_Fruit.swift
@@ -0,0 +1,13 @@
+//
+//  Json_Fruit.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 13.03.2025.
+//
+
+import Foundation
+
+struct Json_Fruit: Codable {
+    var fruitName: String = ""
+    var fruitColor: String = ""
+}
