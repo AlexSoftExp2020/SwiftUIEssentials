ae5722a55929fd90bf893ec8207b39b3bdf18cb6
Alex <oleksii.pidhornyi@gmail.com>
2025-03-13 18:10:21 +0100

    extend json enums bind to picker

--- SwiftUIEssentials/WorkingWithJSON/Json_Enums_BindToPicker.swift
+++ SwiftUIEssentials/WorkingWithJSON/Json_Enums_BindToPicker.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct Json_Enums_BindToPicker: View {
+    @State private var oo = Json_EnumsOO()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("JSON",
+                       subtitle: "Enums - Binding to Picker",
+                       desc: "Example of how to decode JSON itno enums and bind enum to a Picker.",
+                       back: .blue,
+                       textColor: .white)
+            
+            Text(oo.carData.name)
+            
+            Picker("Car Type", selection: $oo.carData.type) {
+                ForEach(CarType.allCases) { type in
+                    Text(type.rawValue.capitalized)
+                        .tag(type)
+                }
+            }
+            .pickerStyle(.segmented)
+            .padding()
+            
+            Text(oo.carData.type.rawValue.capitalized)
+            Text(oo.jsonError?.localizedDescription ?? "")
+        }
+        .onAppear {
+            oo.fetch()
+        }
+        .font(.title)
     }
 }
 
