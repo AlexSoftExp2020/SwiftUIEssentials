e3fc35bfe005c403ebb92ec623789e27ef4f7092
Alex <oleksii.pidhornyi@gmail.com>
2025-03-13 20:40:30 +0100

    add drink order

---
+++ SwiftUIEssentials/WorkingWithJSON/Data/DrinkOrder.swift
@@ -0,0 +1,14 @@
+//
+//  DrinkOrder.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 13.03.2025.
+//
+
+import Foundation
+
+struct DrinkOrder: Codable {
+    var drink = ""
+    var milk: String?
+    var sugarCubes: Int = 0
+}
