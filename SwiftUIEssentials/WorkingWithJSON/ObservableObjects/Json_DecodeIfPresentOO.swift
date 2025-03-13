62c7d8ab7b8825c2089845feb94b6408c072a744
Alex <oleksii.pidhornyi@gmail.com>
2025-03-13 20:54:47 +0100

    add json drink order 1

---
+++ SwiftUIEssentials/WorkingWithJSON/ObservableObjects/Json_DecodeIfPresentOO.swift
@@ -0,0 +1,16 @@
+//
+//  Json_DecodeIfPresentOO.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 13.03.2025.
+//
+
+import Foundation
+
+var jsonDrinkOrder1 = """
+{
+    "drink": "Latte",
+    "milk": "Whole",
+    "sugarCubes": 2
+}
+"""
