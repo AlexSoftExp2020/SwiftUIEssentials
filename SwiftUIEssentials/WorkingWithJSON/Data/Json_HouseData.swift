e091e8dbf9b14f8bcc63763e64087ca200e319c1
Alex <oleksii.pidhornyi@gmail.com>
2025-03-14 17:06:02 +0100

    add json house data

---
+++ SwiftUIEssentials/WorkingWithJSON/Data/Json_HouseData.swift
@@ -0,0 +1,14 @@
+//
+//  Json_HouseData.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 14.03.2025.
+//
+
+import Foundation
+
+struct Json_HouseData: Decodable {
+    var owner = ""
+    var street = ""
+    var city = ""
+}
