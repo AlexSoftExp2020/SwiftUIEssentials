0ffff203a7712251c7897bc631fad5ef85f2c7b7
Alex <oleksii.pidhornyi@gmail.com>
2025-03-14 17:31:25 +0100

    add json house data

---
+++ SwiftUIEssentials/WorkingWithJSON/ObservableObjects/Json_CustomDecodingOO.swift
@@ -0,0 +1,18 @@
+//
+//  Json_CustomDecodingOO.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 14.03.2025.
+//
+
+import SwiftUI
+
+var jsonHouseData = """
+{
+    "owner": "Emin Jacobsen",
+    "address": {
+        "street": "123 Main St."
+        "city": "Sundsvall"
+    }
+}
+"""
