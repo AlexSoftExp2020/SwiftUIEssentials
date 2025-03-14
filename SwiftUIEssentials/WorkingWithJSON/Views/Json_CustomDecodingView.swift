5dc9595ef77d720c1334906a95485ea7801b4dd4
Alex <oleksii.pidhornyi@gmail.com>
2025-03-14 17:41:32 +0100

    add json custom decoding view

---
+++ SwiftUIEssentials/WorkingWithJSON/Views/Json_CustomDecodingView.swift
@@ -0,0 +1,18 @@
+//
+//  Json_CustomDecodingView.swift
+//  SwiftUIEssentials
+//
+//  Created by Oleksii on 14.03.2025.
+//
+
+import SwiftUI
+
+struct Json_CustomDecodingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Json_CustomDecodingView()
+}
